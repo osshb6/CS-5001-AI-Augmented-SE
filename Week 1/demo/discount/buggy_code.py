5ec250b141a55c4f0ed4197ff8e197cf6ad06ecf
Oscar Sargent <osshb6@mst.edu>
2026-02-05 12:33:33 -0600

    Remove unnecessary files for in class activity 2

--- Week 1/demo/discount/buggy_code.py
+++
@@ -1,7 +0,0 @@
-def calculate_total(items, discount):
-    total = 0
-    for item in items:
-        total += item["price"] * item["quantity"]
-    if discount:
-        total = total - discount
-    return total
